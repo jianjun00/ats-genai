14d93108366e8da104350cbc9b76d517d1bbde82
jjchen762 <88948788+jjchen762@users.noreply.github.com>
2025-08-07 05:20:54 -0700

    Add missing proto and generated Python files for factor, indicator, instrument, and time duration intervals. Add state/factor_interval.py and update proto sources.

---
+++ src/state/factor_interval.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass, field
+from typing import Dict
+from state.instrument_interval import InstrumentInterval
+
+from datetime import datetime
+
+@dataclass
+class FactorInterval:
+    start_date_time: datetime
+    end_date_time: datetime
+    instrument_intervals: Dict[int, InstrumentInterval] = field(default_factory=dict)
